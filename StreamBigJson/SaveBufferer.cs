9863891a031a7abf791a67bb8db343c45d25e6b2
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-07 23:23:50 +0100

    keep track of total buffered item count

--- StreamBigJson/SaveBufferer.cs
+++ StreamBigJson/SaveBufferer.cs
@@ -5,16 +5,20 @@ namespace StreamBigJson;
 public class SaveBufferer(DbContext context, int threshold)
 {
     private int _counter = 0;
+    private int _totalCounter = 0;
 
     public async Task BufferedSave()
     {
+        _totalCounter++;
         if (++_counter < threshold)
             return;
         await Save();
+        _counter = 0;
     }
 
     public async Task Save()
     {
         await context.SaveChangesAsync();
+        Console.WriteLine($"Saved {_totalCounter} package records");
     }
 }
